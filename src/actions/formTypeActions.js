a9b22e93d313ea2ce93642c25592adef7605cb8b
mrwerner392 <mrwerner392@gmail.com>
2019-10-31 20:21:22 -0400

    started create profile form inputs

--- src/actions/formTypeActions.js
+++
@@ -1,3 +0,0 @@
-export const toggleAccountFormType = formType => dispatch => {
-  dispatch({ type: 'TOGGLE_ACCOUNT_FORM', formType })
-}
--- src/components/AccountForm.js
+++ src/components/AccountForm.js
@@ -1,16 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class AccountForm extends Component {
 
   state = {
     username: '',
-    password: ''
+    password: '',
+    age: null,
+    bio: '',
+    college: '',
+    occupation: '',
+    interests: []
   }
 
   handleChange = evt => {
-    this.setState({
-      [evt.target.name]: evt.target.value
-    });
+    if (evt.target.name === 'interests') {
+      this.setState(prevState => {
+        return {
+          ...prevState,
+          interests: [...prevState.interests, evt.target.value]
+        }
+      })
+    } else {
+      this.setState({
+        [evt.target.name]: evt.target.value
+      })
+    };
   }
 
   handleSubmit = evt => {
@@ -19,17 +33,54 @@ class AccountForm extends Component {
     this.props.userLoginRequest(this.state)
   }
 
+  renderCreateProfileFormInputs = () => {
+    const { age, bio, college, occupation, interests } = this.state
+    return (
+      <Fragment>
+        <input type='number'
+                name='age'
+                value={ age }
+                placeholder='username'
+                />
+        <input type='text'
+                name='bio'
+                value={ bio }
+                placeholder='username'
+                />
+        <input type='text'
+                name='college'
+                value={ college }
+                placeholder='username'
+                />
+        <input type='text'
+                name='occupation'
+                value={ occupation }
+                placeholder='username'
+                />
+        <label htmlFor='user-interest-select'>Choose up to 5 Interests</label>
+        <select id='user-interest-select' name='interests'>
+          { renderInterestOptions() }
+        </select>
+        <p>{ interests.join(', ') }</p>
+      </Fragment>
+    )
+
+  }
+
   render() {
+    const { state: {username, password},
+            handleChange,
+            handleSubmit } = this
     return (
       <form onChange={ this.handleChange } onSubmit={ this.handleSubmit }>
         <input type='text'
                 name='username'
-                value={ this.state.value }
+                value={ username }
                 placeholder='username'
                 />
         <input type='password'
                 name='password'
-                value={ this.state.value }
+                value={ password }
                 placeholder='password'
                 />
         <input type='submit' />
