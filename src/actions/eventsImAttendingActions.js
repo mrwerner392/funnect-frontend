26ee0c5e50cce55caab3260314b64020bc801b8f
mrwerner392 <mrwerner392@gmail.com>
2019-11-02 12:51:07 -0400

    events attending actions

---
+++ src/actions/eventsImAttendingActions.js
@@ -0,0 +1,20 @@
+const URL = 'http://localhost:3000'
+
+const setEvents = events => {
+  return {
+    type: 'SET_EVENTS_ATTENDING',
+    events
+  }
+}
+
+export const getEventsAttending = () => dispatch => {
+  const config = {
+    headers: {
+      'Authorization': localStorage.token
+    }
+  }
+
+  fetch(URL + `users/${localStorage.id}/events_attended`)
+  .then(res => res.json())
+  .then(console.log)
+}
