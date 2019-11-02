d2eb10fbfdece8eaef60d56b8080fbd28ac42a08
mrwerner392 <mrwerner392@gmail.com>
2019-11-02 17:15:33 -0400

    interests reducer

---
+++ src/reducers/interestsReducer.js
@@ -0,0 +1,11 @@
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'SET_INTERESTS':
+      return [
+        ...state,
+        action.interests
+      ]
+    default:
+      return state
+  }
+}
