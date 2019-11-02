ed868bb2ac87f184b35fd891d9de0d3d6abe5ea4
mrwerner392 <mrwerner392@gmail.com>
2019-11-02 18:02:55 -0400

    set available posts filter action

--- src/actions/availablePostsActions.js
+++ src/actions/availablePostsActions.js
@@ -17,5 +17,8 @@ export const getAvailablePosts = () => dispatch => {
   fetch(URL + `/users/${localStorage.id}/available_posts`, config)
   .then(res => res.json())
   .then(posts => dispatch(setPosts(posts)))
-  // .then(console.log)
+}
+
+export const setAvailablePostsFilter = filter => dispatch => {
+  dispatch({ type: 'SET_AVAILABLE_POSTS_FILTER', filter })
 }
