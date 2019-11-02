308751d41ef641031db33cedf01468f9fc7ccc1d
mrwerner392 <mrwerner392@gmail.com>
2019-11-02 18:04:35 -0400

    set available posts filter reducer

--- src/reducers/availablePostsReducer.js
+++ src/reducers/availablePostsReducer.js
@@ -1,9 +1,14 @@
-export default (state = {posts: []}, action) => {
+export default (state = {posts: [], filter: ''}, action) => {
   switch (action.type) {
     case 'SET_AVAILABLE_POSTS':
       return {
         ...state,
         posts: action.posts
+      }
+    case 'SET_AVAILABLE_POSTS_FILTER':
+      return {
+        ...state,
+        filter: action.filter
       }
     default:
       return state
