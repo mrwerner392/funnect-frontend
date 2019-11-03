3836d4d0ac2376e349afdf357a1876a979e166d4
mrwerner392 <mrwerner392@gmail.com>
2019-11-03 15:20:13 -0500

    message form component template

---
+++ src/components/MesageForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MessageForm extends Component {
+
+  render() {
+    return (
+      <div>MessageForm</div>
+    )
+  }
+
+}
+
+export default MessageForm
