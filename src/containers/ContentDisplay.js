3ecfca10da606e1e71cd394deca0d3fea53bef9f
mrwerner392 <mrwerner392@gmail.com>
2019-11-02 21:29:03 -0400

    removed create event button, will move elsewhere

--- src/containers/ContentDisplay.js
+++ src/containers/ContentDisplay.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import ProfileInfo from '../components/ProfileInfo';
 
 class ContentDisplay extends Component {
