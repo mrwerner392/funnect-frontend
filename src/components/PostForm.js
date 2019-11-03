f0cd846b7128a3134416d1efd968048c0e298061
mrwerner392 <mrwerner392@gmail.com>
2019-11-02 20:11:36 -0400

    post form render neighborhood options

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class PostForm extends Component {
 
+  state = {
+    neighborhood: '',
+    topic: '',
+    description: '',
+    day: '',
+    time_of_day: ''
+  }
+
+  renderNeighborhoodOptions = () => {
+    const { neighborhoods } = this.props
+    return neighborhoods.map(neighborhood => (
+      <option key={ neighborhood.id } value={ neighborhood.id }>{ neighborhood.name }</option>
+    ))
+  }
+
   render() {
+    const { renderNeighborhoodOptions } = this
     return (
-      <div>
-        PostForm
-      </div>
+      <form>
+        Choose Neighborhood:
+        <select>
+          { renderNeighborhoodOptions() }
+        </select>
+      </form>
     )
   }
 
 }
 
-export default PostForm;
+const mapStateToProps = state => {
+  return {
+    neighborhoods: state.neighborhoods
+  }
+}
+
+export default connect(mapStateToProps)(PostForm);
