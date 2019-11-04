abde82dc36a0cd1427c40f1d60ac7eefe1b61d8c
mrwerner392 <mrwerner392@gmail.com>
2019-11-04 12:34:23 -0500

    navlink on profile edit form to cancel

--- src/components/ProfileInfoEditForm.js
+++ src/components/ProfileInfoEditForm.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { setUser } from '../actions/userActions'
 
 const URL = 'http://localhost:3000'
@@ -76,64 +77,66 @@ class ProfileInfoEditForm extends Component {
             handleSubmit } = this
 
     return (
-      <form onChange={ handleChange }
-            onSubmit={ handleSubmit }
-            >
-        { errors ? <p>{ errors }</p> : null}
-        <input type='text'
-                name='username'
-                value={ user.username }
-                placeholder='username'
-                />
-        <input type='text'
-                name='first_name'
-                value={ user.first_name }
-                placeholder='first name'
-                />
-        <input type='number'
-                name='age'
-                value={ user.age }
-                placeholder='age'
-                />
-        <input type='text'
-                name='gender'
-                value={ user.gender }
-                placeholder='gender'
-                />
-        <input type='text'
-                name='bio'
-                value={ user.bio }
-                placeholder='bio'
-                />
-        <input type='text'
-                name='college'
-                value={ user.college }
-                placeholder='college'
-                />
-        <input type='text'
-                name='occupation'
-                value={ user.occupation }
-                placeholder='occupation'
-                />
-        {/*<label htmlFor='user-interest-select'>Choose up to 5 Interests</label>
-        <select id='user-interest-select' name='interests'>
-          { <option></option> }
-          {
-            interestOptions.map(interest => {
-              return <option key={ interest.id } value={ interest.name }>{ interest.name }</option>
-            })
-          }
-        </select>
-        <p>{ interests.join(', ') }</p>*/}
-        <input type='submit' />
-      </form>
+      <Fragment>
+        <form onChange={ handleChange }
+              onSubmit={ handleSubmit }
+              >
+          { errors ? <p>{ errors }</p> : null}
+          <input type='text'
+                  name='username'
+                  value={ user.username }
+                  placeholder='username'
+                  />
+          <input type='text'
+                  name='first_name'
+                  value={ user.first_name }
+                  placeholder='first name'
+                  />
+          <input type='number'
+                  name='age'
+                  value={ user.age }
+                  placeholder='age'
+                  />
+          <input type='text'
+                  name='gender'
+                  value={ user.gender }
+                  placeholder='gender'
+                  />
+          <input type='text'
+                  name='bio'
+                  value={ user.bio }
+                  placeholder='bio'
+                  />
+          <input type='text'
+                  name='college'
+                  value={ user.college }
+                  placeholder='college'
+                  />
+          <input type='text'
+                  name='occupation'
+                  value={ user.occupation }
+                  placeholder='occupation'
+                  />
+          {/*<label htmlFor='user-interest-select'>Choose up to 5 Interests</label>
+          <select id='user-interest-select' name='interests'>
+            { <option></option> }
+            {
+              interestOptions.map(interest => {
+                return <option key={ interest.id } value={ interest.name }>{ interest.name }</option>
+              })
+            }
+          </select>
+          <p>{ interests.join(', ') }</p>*/}
+          <input type='submit' />
+        </form>
+        <NavLink exact to={ `/${ user.username }` } >Cancel</NavLink>
+      </Fragment>
     )
   }
 
 }
 
 const mapStateToProps = state => {
-  console.log(state.user);
   return {
     user: state.user,
     interestOptions: state.interests
