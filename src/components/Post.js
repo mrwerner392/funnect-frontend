dd2a0b020351c25c308aa95e7f62dd3cb8b4e11e
mrwerner392 <mrwerner392@gmail.com>
2019-11-04 15:52:47 -0500

    update details shown in Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,13 +4,23 @@ import { NavLink } from 'react-router-dom';
 
 class Post extends Component {
 
+  renderUserInterests = interests => {
+    const interestNames = interests.map(interest => interest.name)
+    return interestNames.join(', ')
+  }
+
   render() {
-    const { post, user } = this.props
+    const { props: {post, user}, renderUserInterests } = this
+    console.log(post.user);
     return (
       <div className='Post'>
         <p>{ post.topic.name }</p>
         <p>{ post.neighborhood.name }</p>
-        <p>{ post.user.username }</p>
+        <p>{ post.time_of_day }</p>
+        <h6>Poster:</h6>
+        <p>{ `${post.user.username}, ${post.user.age}, ${post.user.occupation}` }</p>
+        <p>{ `"${post.user.bio}"` }</p>
+        <p>Likes: { renderUserInterests(post.user.interests) }</p>
         {
           post.user.id === user.id
           ? <NavLink exact to={ `/${user.username}/posts/${post.id}` }>Manage Post</NavLink>
