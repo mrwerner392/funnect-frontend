f5006ce0cf6f1183044c487fbd617da9731ce521
mrwerner392 <mrwerner392@gmail.com>
2019-11-04 16:10:02 -0500

    more updates to post details rendered

--- src/components/Post.js
+++ src/components/Post.js
@@ -9,23 +9,40 @@ class Post extends Component {
     return interestNames.join(', ')
   }
 
+  renderPostFooter = () => {
+    const { post, user } = this.props
+    const interestedIds = post.interested_users.map(user => user.id)
+
+    if (post.user.id === user.id) {
+      return (
+        <p>{ interestedIds.length } users are interested <span><NavLink exact to={ `/${user.username}/posts/${post.id}` }>Manage Post</NavLink></span></p>
+      )
+    } else if (interestedIds.includes(user.id)) {
+
+    } else {
+
+    }
+  }
+
   render() {
-    const { props: {post, user}, renderUserInterests } = this
+    const { props: {post, user}, renderUserInterests, renderPostFooter } = this
     console.log(post.user);
     return (
       <div className='Post'>
         <p>{ post.topic.name }</p>
         <p>{ post.neighborhood.name }</p>
         <p>{ post.time_of_day }</p>
-        <h6>Poster:</h6>
+        <p>{ post.description }</p>
+        <h4>Poster:</h4>
         <p>{ `${post.user.username}, ${post.user.age}, ${post.user.occupation}` }</p>
         <p>{ `"${post.user.bio}"` }</p>
         <p>Likes: { renderUserInterests(post.user.interests) }</p>
-        {
+        { renderPostFooter() }
+        {/*
           post.user.id === user.id
           ? <NavLink exact to={ `/${user.username}/posts/${post.id}` }>Manage Post</NavLink>
           : null
-        }
+        */}
       </div>
     )
   }
