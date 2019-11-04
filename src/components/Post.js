e3cb3b20d518593b6d87072249e03069b33c3948
mrwerner392 <mrwerner392@gmail.com>
2019-11-04 16:40:13 -0500

    click handler templates for interested and not interested buttons

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,13 +4,23 @@ import { NavLink } from 'react-router-dom';
 
 class Post extends Component {
 
+  handleInterestedClick = id => {
+    console.log('interested', id);
+  }
+
+  handleNotInterestedClick = id => {
+    console.log('not interested', id)
+  }
+
   renderUserInterests = interests => {
     const interestNames = interests.map(interest => interest.name)
     return interestNames.join(', ')
   }
 
   renderPostFooter = () => {
-    const { post, user } = this.props
+    const { props: {post, user},
+            handleInterestedClick,
+            handleNotInterestedClick } = this
     const interestedIds = post.interested_users.map(user => user.id)
 
     if (post.user.id === user.id) {
@@ -19,11 +29,11 @@ class Post extends Component {
       )
     } else if (interestedIds.includes(user.id)) {
       return (
-        <p>You and { interestedIds.length - 1 } are interested<span><button>{ "I'm Not Interested" }</button></span></p>
+        <p>You and { interestedIds.length - 1 } are interested<span><button onClick={ () => handleNotInterestedClick(post.id) }>{ "I'm Not Interested" }</button></span></p>
       )
     } else {
       return (
-        <p>{ interestedIds.length } users are interested <span><button>{ "I'm Interested" }</button></span></p>
+        <p>{ interestedIds.length } users are interested <span><button onClick={ () => handleInterestedClick(post.id) }>{ "I'm Interested" }</button></span></p>
       )
     }
   }
