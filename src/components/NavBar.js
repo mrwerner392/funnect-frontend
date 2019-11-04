5226e818715991dfcdb5eee38391b1a6b7527c31
mrwerner392 <mrwerner392@gmail.com>
2019-11-04 09:22:44 -0500

    nav link to user page in nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 class NavBar extends Component {
 
   render() {
+    const { username } = this.props.user
     return (
-      <div>NavBar</div>
+      <div>
+        <NavLink exact to={ `/${username}` } >{ username }</NavLink>
+      </div>
     )
   }
 
 }
 
-export default NavBar
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(NavBar)
