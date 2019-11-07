a7556be9846426d02624832e44c76b40e76347eb
mrwerner392 <mrwerner392@gmail.com>
2019-11-06 19:30:50 -0500

    current event reducer

---
+++ src/reducers/currentEventReducer.js
@@ -0,0 +1,10 @@
+export default (state = {}, action) => {
+  switch (action.type) {
+    case 'SET_CURRENT_EVENT':
+      return {
+        ...action.event
+      }
+    default:
+      state
+  }
+}
