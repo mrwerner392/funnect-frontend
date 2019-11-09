8740ca59aed6cda6a9bffaa4bcd17a86c55117a0
mrwerner392 <mrwerner392@gmail.com>
2019-11-09 14:25:55 -0500

    also clear new interested users on current post in state

--- src/reducers/currentPostReducer.js
+++ src/reducers/currentPostReducer.js
@@ -1,9 +1,20 @@
 export default (state = {}, action) => {
   switch (action.type) {
     case 'SET_CURRENT_POST':
-      return {
-        ...action.post
-      }
+      return action.post.newInterestedUsers
+                  ?
+                    {
+                      ...action.post,
+                      interested_users: [
+                        ...action.post.interested_users,
+                        ...action.post.newInterestedUsers
+                      ],
+                      newInterestedUsers: []
+                    }
+                  :
+                    {
+                      ...action.post
+                    }
     default:
       return state
   }
