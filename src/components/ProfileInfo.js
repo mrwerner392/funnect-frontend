ce639bb155c4da970b5a68616492138ce7bae3f0
mrwerner392 <mrwerner392@gmail.com>
2019-11-17 22:42:50 -0500

    refactor ProfileInfo from class to function component

--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -3,50 +3,42 @@ import { NavLink, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setContentType } from '../actions/contentTypeActions';
 
-class ProfileInfo extends Component {
+const ProfileInfo = props => {
 
-  renderInterests = () => {
-    const { interests } = this.props.user
+  const { username,
+          first_name,
+          age,
+          gender,
+          bio,
+          college,
+          occupation,
+          interests } = props.user
+
+  const renderInterests = () => {
     const interestNames = interests.map(interest => interest.name)
     return interestNames.join(', ')
   }
 
-  render() {
-    const { props: {
-              user: {
-                username,
-                first_name,
-                age,
-                gender,
-                bio,
-                college,
-                occupation
-              }
-            },
-            renderInterests,
-            handleEditProfileClick } = this
-
-    return (
-      <div id='profile-info-display'>
-        <h4 className='profile-info-label'>Username</h4>
-        <p className='profile-info-item' id='profile-username'>{ username }</p>
-        <h4 className='profile-info-label'>First Name</h4>
-        <p className='profile-info-item' id='profile-first-name'>{ first_name }</p>
-        <h4 className='profile-info-label'>Age</h4>
-        <p className='profile-info-item' id='profile-age'>{ age }</p>
-        <h4 className='profile-info-label'>Gender</h4>
-        <p className='profile-info-item' id='profile-gender'>{ gender }</p>
-        <h4 className='profile-info-label'>Bio</h4>
-        <p className='profile-info-item' id='profile-bio'>{ bio }</p>
-        <h4 className='profile-info-label'>College</h4>
-        <p className='profile-info-item' id='profile-college'>{ college }</p>
-        <h4 className='profile-info-label'>Occupation</h4>
-        <p className='profile-info-item' id='profile-occupation'>{ occupation }</p>
-        <h4 className='profile-info-label'>Interests</h4>
-        <p className='profile-info-item profile-info-bottom' id='profile-interests'>{ renderInterests() }</p>
-      </div>
-    )
-  }
+  return (
+    <div id='profile-info-display'>
+      <h4 className='profile-info-label'>Username</h4>
+      <p className='profile-info-item' id='profile-username'>{ username }</p>
+      <h4 className='profile-info-label'>First Name</h4>
+      <p className='profile-info-item' id='profile-first-name'>{ first_name }</p>
+      <h4 className='profile-info-label'>Age</h4>
+      <p className='profile-info-item' id='profile-age'>{ age }</p>
+      <h4 className='profile-info-label'>Gender</h4>
+      <p className='profile-info-item' id='profile-gender'>{ gender }</p>
+      <h4 className='profile-info-label'>Bio</h4>
+      <p className='profile-info-item' id='profile-bio'>{ bio }</p>
+      <h4 className='profile-info-label'>College</h4>
+      <p className='profile-info-item' id='profile-college'>{ college }</p>
+      <h4 className='profile-info-label'>Occupation</h4>
+      <p className='profile-info-item' id='profile-occupation'>{ occupation }</p>
+      <h4 className='profile-info-label'>Interests</h4>
+      <p className='profile-info-item profile-info-bottom' id='profile-interests'>{ renderInterests() }</p>
+    </div>
+  )
 
 }
 
