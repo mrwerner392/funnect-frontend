988ae43369803b75d07f805192991c884ef25bb7
mrwerner392 <mrwerner392@gmail.com>
2019-11-18 19:56:10 -0500

    bug fix with post channel cable refactoring

--- src/components/PostChannelCable.js
+++ src/components/PostChannelCable.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { addPostWaiting } from '../actions/availablePostsActions';
 import { addCreatedPost } from '../actions/myCreatedPostsActions';
 import { ActionCableConsumer } from 'react-actioncable-provider';
