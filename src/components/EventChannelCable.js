4b8bac7b7d7a7434ed56c7a20391405973488705
mrwerner392 <mrwerner392@gmail.com>
2019-11-26 15:18:56 -0500

    event channel cable comments

--- src/components/EventChannelCable.js
+++ src/components/EventChannelCable.js
@@ -3,8 +3,10 @@ import { connect } from 'react-redux';
 import { newEventAttending } from '../actions/eventsImAttendingActions';
 import { ActionCableConsumer } from 'react-actioncable-provider';
 
+// Action Cable for all events web socket channel
 const EventChannelCable = ({ user, newEventAttending }) => {
 
+  // update redux state with new event if this user was invited
   const handleNewEvent = event => {
     const attendingIds = event.users_attending.map(user => user.id)
     if (attendingIds.includes(user.id)) {
